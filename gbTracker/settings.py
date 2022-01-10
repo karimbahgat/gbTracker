6b5984600c235e043cd428e262c67d113ba01123
Karim Bahgat <karim.bahgat.norway@gmail.com>
2022-01-10 16:29:38 +0100

    Copy geoboundaries website style setup

--- gbTracker/settings.py
+++ gbTracker/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'gbTracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# non-app specific static folders
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- gbTracker/views.py
+++ gbTracker/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'templates/home.html')
