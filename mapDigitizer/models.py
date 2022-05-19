7a5f4bf8e9ba170bf2b503b1a780e3857136ead7
Karim Bahgat <karim.bahgat.norway@gmail.com>
2022-05-19 22:26:09 +0200

    Add MapDigitizer model

--- mapDigitizer/models.py
+++ mapDigitizer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class MapDigitizer(models.Model):
+    source = models.OneToOneField("changeManager.BoundarySource", related_name='digitizer', on_delete=models.PROTECT)
+    digitized_data = models.JSONField(blank=True, null=True)
+    last_digitized = models.DateTimeField(null=True, blank=True)
