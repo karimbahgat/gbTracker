20a9a244de60b967f803e1cbf3c04933afa5e3c9
Karim Bahgat <karim.bahgat.norway@gmail.com>
2022-06-09 18:07:01 +0200

    Homepage list and link to each country build page, with useful info on sources and timeperiod

--- gbTracker/views.py
+++ gbTracker/views.py
@@ -1,26 +1,44 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Count, Min, Max
 
-from changeManager.models import BoundaryReference, BoundarySnapshot
+from changeManager.models import BoundaryReference, BoundarySnapshot, BoundarySource
 
 import json
 
 def home(request):
     toprefs = BoundaryReference.objects.filter(parent=None)
     if toprefs:
-        countries = set()
+        countrynames = set()
         countries_geoj = {'type':'FeatureCollection', 'features':[]}
         for ref in toprefs:
             name = ref.names.first().name
-            countries.add(name)
+            countrynames.add(name)
             #snap = ref.snapshots.first()
             #if snap:
             #    geom = snap.geom.__geo_interface__
             #    props = {'name':name}
             #    feat = {'type':'Feature', 'geometry':geom, 'properties':props}
             #    countries_geoj['features'].append(feat)
-        countries = sorted(countries)
+        countrynames = sorted(countrynames)
+        countries = []
+        for country in countrynames:
+            print(country)
+            levels = []
+            for lvl in [0,1,2]:
+                parent_param = ['parent']*lvl
+                filter_param = '__'.join( ['boundary_refs'] + parent_param + ['names','name'] )
+                filter_kwargs = {}
+                filter_kwargs[filter_param] = country
+                sources = BoundarySource.objects.filter(**filter_kwargs).distinct().count()
+                agg = BoundarySource.objects.filter(**filter_kwargs).aggregate(mindate=Min('boundary_refs__snapshots__event__date_start'), maxdate=Max('boundary_refs__snapshots__event__date_end'))
+                info = {'name':country, 'level':lvl, 'sources':sources}
+                info.update(agg)
+                print(info)
+                levels.append(info)
+            print(levels)
+            countries.append(levels)
         countries_geoj = json.dumps(countries_geoj)
     else:
         countries = []
